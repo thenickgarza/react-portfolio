251281283d27259d2232fe46c952087be152ac8a
Nick Garza <thenickgarza14@gmail.com>
2022-06-16 16:33:16 -0500

    added the homepage word feeature

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
-
+import Home from './components/Home';
 function App() {
   return (
+    <main>
     <Nav></Nav>
+    <Home></Home>
+    </main>
   );
 }
 
---
+++ client/src/components/Home/index.js
@@ -0,0 +1,67 @@
+import { render } from "@testing-library/react";
+import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import LogoLG from "../../assets/images/ce-logo-wht.png";
+
+const textArray1 = ['Hello!', 'Hola!', 'Welcome!'];
+const textArray2 = ['Full-Stack Web Developer', 'Innovator Of Ideas', 'Your Guy!',];
+
+const Home = () => {
+    const [index1, setIndex1] = useState(0);
+    const [index2, setIndex2] = useState(0);
+
+    useEffect(() => {
+        const timer = () => {
+          setIndex1(prevIndex => {
+            if(prevIndex === textArray1.length - 1){
+              return 0;
+            } 
+            return prevIndex + 1;
+          })
+        };
+        setInterval(timer, 5000);
+        
+        //cleanup function in order clear the interval timer
+        //when the component unmounts
+        return () => { clearInterval(timer); }
+      }, []);
+
+      useEffect(() => {
+        const timer = () => {
+          setIndex2(prevIndex => {
+            if(prevIndex === textArray2.length - 1){
+              return 0;
+            } 
+            return prevIndex + 1;
+          })
+        };
+        setInterval(timer, 7000);
+        
+        //cleanup function in order clear the interval timer
+        //when the component unmounts
+        return () => { clearInterval(timer); }
+      }, []);
+    
+    
+return (
+<div className="container-fluid">
+    <div className="row align-items-center">
+        <div className="col-lg-6 home-text-container">
+            <span className="welcome-text">{textArray1[index1]}</span>
+            <span className="intro-text">I'm Nicholas Garza,</span>
+            <span className="job-text">{textArray2[index2]}</span>
+            {/* <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link> */}
+        </div>
+        <div className="col-lg-6 home-logo-container">
+        {/* <img className="home-logo" src={LogoLG} alt="CAE" /> */}
+        </div>
+        
+    </div>
+</div>
+  );
+};
+
+export default Home;
+
