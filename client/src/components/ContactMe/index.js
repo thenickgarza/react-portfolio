36f414a191292dea1ee40f904506f32ed7b39032
Nick Garza <thenickgarza14@gmail.com>
2022-06-23 22:34:41 -0500

    added screenshots of my work to the cards

--- client/src/components/ContactMe/index.js
+++ client/src/components/ContactMe/index.js
@@ -5,9 +5,24 @@ import React from "react";
 function ContactMe() {
 
     return (
-        <div className="contactmebox">
-            <h1 style={{color: 'white'}}>Contact Me</h1>
-        </div>
+        <section className="contactmecontainer">
+            <form id="contact-form">
+                <div>
+                <h1 style={{textAlign: "center"}}>Contact me</h1>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                
+                
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" style={{    width: "-webkit-fill-available"}} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" rows="5"/>
+                </div>
+                <button type="submit" style={{justifyContent: "center"}}>Send! </button>
+            </form>
+        </section>
     )
 }
 
--- client/src/components/MyWork/index.js
+++ client/src/components/MyWork/index.js
@@ -3,6 +3,8 @@ import Card from 'react-bootstrap/Card'
 import Button from "react-bootstrap/Button";
 // import ngLogo from '../../assets/images/ngLogo.png'
 import SmartHome from '../../assets/images/Smart-Home.png'
+import AuctionHouse from '../../assets/images/AuctionHouse.png'
+import Homepage from '../../assets/images/Homepage.png'
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
@@ -18,8 +20,9 @@ function MyWork() {
       <Card.Body>
         <Card.Title>SmartHome</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
+          SmartHome is a project built with React and GraphQL as the database. 
+          The site was made to provide "smart home" services to customers 
+          while creating jobs for providers.
         </Card.Text>
         <Button variant="primary">Live Url</Button>
         <Button variant="primary">View Github</Button>
@@ -29,7 +32,7 @@ function MyWork() {
     
     <Col>
     <Card className="card" style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <Card.Img variant="top" src={Homepage} />
       <Card.Body>
         <Card.Title>Custom Homepage</Card.Title>
         <Card.Text>
@@ -43,9 +46,9 @@ function MyWork() {
     </Col>
     <Col>
     <Card className="card" style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <Card.Img variant="top" src={AuctionHouse} />
       <Card.Body>
-        <Card.Title>Weather App</Card.Title>
+        <Card.Title>Auction House</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the bulk of
           the card's content.
