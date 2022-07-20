afc40d9fbc2da125a1a0178739cddd94a45cd397
Nick Garza <thenickgarza14@gmail.com>
2022-07-20 16:21:43 -0500

    back to back fails

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 
