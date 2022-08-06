b4989d1f31dbc537ee17d6ce1ec50a3a12d869d4
Nick Garza <thenickgarza14@gmail.com>
2022-08-06 15:27:33 -0500

    trying with the nav component directly in the app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import './App.css';
 import React from 'react';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './components/About';
-import MyWork from './components/MyWork';
-import ContactMe from './components/ContactMe'
+// import MyWork from './components/MyWork';
+// import ContactMe from './components/ContactMe'
 import Home from './components/Home';
 // import Projects from './components/MyWork';
 function App() {
 
   return (
-    <>
-    <Router>
-      <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="mywork" element={<MyWork />} />
-          <Route path="contactme" element={<ContactMe/>} />
-        </Routes>
-    </Router>
-    </>
+    <header className="flex-row">
+      <h2>
+        <a
+          className="home-name"
+          href="/"
+          style={{ color: "#3bc14a", marginLeft: "15px" }}
+        ><Link to="/"></Link>
+          <FaHome />
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a className="nav-text" href="/about" style={{ color: "#3bc14a" }}><Link to="/about">
+             </Link> About Me
+            </a>
+          </li>
+          <li className="mx-2">
+            <a className="nav-text" href="/mywork" style={{ color: "#3bc14a" }}><Link to="/mywork"></Link>
+              My Work
+            </a>
+          </li>
+          <li className="mx-2">
+            <a
+              className="nav-text"
+              href="/contactme"
+              style={{ color: "#3bc14a" }}
+            ><Link to="/contactme"></Link>
+              Contact Me
+            </a>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </nav>
+    </header>
+    
+    
+    
+    
+    
+    
+    
+    
+    // <>
+    // <Router>
+    //   <Nav />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="about" element={<About />} />
+    //       <Route path="mywork" element={<MyWork />} />
+    //       <Route path="contactme" element={<ContactMe/>} />
+    //     </Routes>
+    // </Router>
+    // </>
    )
 }
 
