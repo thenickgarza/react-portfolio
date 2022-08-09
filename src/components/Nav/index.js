c0f19e0b15a68cde9807e58d0a3a0d3ab73e5dff
Nick Garza <thenickgarza14@gmail.com>
2022-08-09 13:04:54 -0500

    fixed the color issue in the nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,44 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+// const textArray1 = ["Hi!", "Welcome!", "Hola!"];
+// const textArray2 = ["Full-Stack Web Developer", "Innovator Of Ideas", "Father"];
+
+// const Home = () => {
+//   const [index1, setIndex1] = useState(0);
+//   const [index2, setIndex2] = useState(0);
+
+//   useEffect(() => {
+//     const timer = () => {
+//       setIndex1((prevIndex) => {
+//         if (prevIndex === textArray1.length - 1) {
+//           return 0;
+//         }
+//         return prevIndex + 1;
+//       });
+//     };
+//     setInterval(timer, 5000);
+
+//     //cleanup function in order clear the interval timer
+//     //when the component unmounts
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+
+  // useEffect(() => {
+  //   const timer = () => {
+  //     setIndex2((prevIndex) => {
+  //       if (prevIndex === textArray2.length - 1) {
+  //         return 0;
+  //       }
+  //       return prevIndex + 1;
+  //     });
+  //   };
+  //   setInterval(timer, 7000);
+  // })
 
 const Nav = () => {
   return (
@@ -10,18 +48,18 @@ const Nav = () => {
           className="home-name"
           href="/"
           style={{ color: "#3bc14a", marginLeft: "15px" }}
-        ><Link to="/"><FaHome /></Link>
+        ><Link to="/" style={{ color: "#3bc14a" }}><FaHome /></Link>
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a className="nav-text" href="/about" style={{ color: "#3bc14a" }}><Link to="/about">
+            <a className="nav-text" href="/about" style={{ color: "#3bc14a" }}><Link to="/about" style={{ color: "#3bc14a" }}>
             About Me</Link>
             </a>
           </li>
           <li className="mx-2">
-            <a className="nav-text" href="/mywork" style={{ color: "#3bc14a" }}><Link to="/mywork">My Work</Link>
+            <a className="nav-text" href="/mywork" style={{ color: "#3bc14a" }}><Link to="/mywork" style={{ color: "#3bc14a" }}>My Work</Link>
             </a>
           </li>
           <li className="mx-2">
@@ -29,12 +67,16 @@ const Nav = () => {
               className="nav-text"
               href="/contactme"
               style={{ color: "#3bc14a" }}
-            ><Link to="/contactme">Contact Me</Link>
+            ><Link to="/contactme" style={{ color: "#3bc14a" }}>Contact Me</Link>
             </a>
           </li>
         </ul>
       </nav>
+    
+    
+    
     </header>
+    
   );
 };
 
